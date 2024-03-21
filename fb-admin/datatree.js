2c9819a8ee71dbd339eec4eae198417cc156525c
Jerry <jerry_agboola@hotmail.com>
2024-03-21 15:53:47 +0100

    firebase script wip

--- fb-admin/datatree.js
+++ fb-admin/datatree.js
@@ -1,23 +1,22 @@
-var admin = require("firebase-admin");
+var admin = require('firebase-admin');
 
 // Fetch the service account key JSON file contents
-var serviceAccount = require( "./starthack2024-firebase-adminsdk-jupxu-5a3edb365a.json");
+var serviceAccount = require('./starthack2024-firebase-adminsdk-jupxu-5a3edb365a.json');
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   // The database URL depends on the location of the database
-  databaseURL: "https://starthack2024-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL: 'https://starthack2024-default-rtdb.europe-west1.firebasedatabase.app',
 });
-console.log("Program started")
+console.log('Program started');
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 const db = admin.database();
-const ref = db.ref("/alerts");
-const nursesRef = db.ref("/nurses")
-const bedsRef = db.ref("/beds")
+const ref = db.ref('/alerts');
+const nursesRef = db.ref('/nurses');
+const bedsRef = db.ref('/beds');
 
-const distance = {}
-const allNurses = {}
-const allBeds = {}
+const allNurses = {};
+const allBeds = {};
 
 // Function to calculate the distance between two points
 function calculateDistance(x1, y1, x2, y2) {
@@ -25,13 +24,14 @@ function calculateDistance(x1, y1, x2, y2) {
 }
 
 // Function to choose the nearest nurse based on the provided x and y coordinates
-// Function to choose the nearest nurse based on the provided x and y coordinates
 function choose_nurse(x, y, alertLevel) {
   let minDistance = Infinity;
   let nearestNurse = null;
-  console.log(Object.keys(allNurses).filter(a => allNurses[a].level >= alertLevel))
 
-  for (const nurseId in Object.keys(allNurses).filter(a => allNurses[a].level >= alertLevel).sort((a,b) => a.level - b.level)) {
+  Object.keys(allNurses)
+    .filter((a) => allNurses[a].level >= alertLevel)
+    .sort((a, b) => allNurses[a].level - allNurses[b].level)
+    .map((nurseId) => {
       const nurse = allNurses[nurseId];
       const nurseX = nurse.xPos;
       const nurseY = nurse.yPos;
@@ -39,50 +39,62 @@ function choose_nurse(x, y, alertLevel) {
 
       // Check if the nurse is available
       if (dist < minDistance && nurse.available) {
-          minDistance = dist;
-          nearestNurse = nurseId;
+        minDistance = dist;
+        nearestNurse = nurseId;
       }
-  }
+    });
 
   return nearestNurse;
 }
 
-nursesRef.on("child_added", (snapshot) => {
-  allNurses[snapshot.key] = snapshot.val()
-  console.log(allNurses)
-})
-
-bedsRef.on("child_changed", (snapshot) => {
-  allBeds[snapshot.key] = snapshot.val()
-})
-bedsRef.on("child_added", (snapshot) => {
-  allBeds[snapshot.key] = snapshot.val()
-})
-nursesRef.on("child_changed", (snapshot) => {
-  allNurses[snapshot.key] = snapshot.val()
-})
-
-
-//
-// Listener for changes in alerts
-ref.on("child_added", function(snapshot) {
-  const alert = snapshot.val();
-
-  bed_found = allBeds[alert.bed]
-
-  console.log(bed_found.xPos);
-  console.log(bed_found.yPos);
-  console.log(alert.level);
-  const nearestNurse = choose_nurse(bed_found.xPos, bed_found.yPos, alert.level);
-  
-  
-  if (nearestNurse) {
-      console.log("Nearest nurse:", nearestNurse);
-      // Perform actions with the nearest nurse
-      // For example, you can update the nurse's availability status or assign tasks.
-  } else {
-      console.log("No available nurse found.");
-  }
+nursesRef.on('child_added', (snapshot) => {
+  allNurses[snapshot.key] = snapshot.val();
+  console.log(allNurses);
 });
-//
+nursesRef.on('child_changed', (snapshot) => {
+  allNurses[snapshot.key] = snapshot.val();
+});
 
+bedsRef.on('child_changed', (snapshot) => {
+  allBeds[snapshot.key] = snapshot.val();
+});
+bedsRef.on('child_added', (snapshot) => {
+  allBeds[snapshot.key] = snapshot.val();
+});
+
+nursesRef
+  .once('value', (snapshot) => {
+    snapshot.forEach((s) => {
+      allNurses[s.key] = s.val();
+    });
+  })
+  .then(() => {
+    bedsRef
+      .once('value', (snapshot) => {
+        snapshot.forEach((s) => {
+          allBeds[s.key] = s.val();
+        });
+      })
+      .then(() => {
+        // Listener for changes in alerts
+        ref.on('child_added', function (snapshot) {
+          const alert = snapshot.val();
+
+          bed_found = allBeds[alert.bed];
+
+          const nearestNurse = choose_nurse(bed_found.xPos, bed_found.yPos, alert.level);
+
+          if (nearestNurse) {
+            console.log('Nearest nurse:', nearestNurse);
+
+            // Assign alert to nearest nurse
+            const nearestNurseRef = db.ref('/nurses/' + nearestNurse + '/alerts');
+            nearestNurseRef.update({ [snapshot.key]: alert });
+            nearestNurseRef.parent.update({ available: false });
+          } else {
+            console.log('No available nurse found.');
+          }
+        });
+      });
+  });
+//
