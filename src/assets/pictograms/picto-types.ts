7dfc256641f398565ff99d1ae4e45bbedc1d2176
Jeremy Mrzyglocki <j.mrzyglocki@outlook.de>
2024-03-21 08:01:56 +0100

    added all icons

--- src/assets/pictograms/picto-types.ts
+++ src/assets/pictograms/picto-types.ts
@@ -4,11 +4,76 @@ export const PictoTypes = {
     pictogram: '01_nurse.png',
   },
   support: {
-    desc: 'Unterstützung',
+    desc: 'Support',
     pictogram: '02_support.png',
   },
   pain: {
-    desc: 'Schmerzen',
+    desc: 'Pain',
     pictogram: '03_pain.png',
   },
+  infusion: {
+    desc: 'Infusion',
+    pictogram: '04_infusion.png',
+  },
+  drink: {
+    desc: 'Drink',
+    pictogram: '05_drink.png',
+  },
+  toilet: {
+    desc: 'Toilet',
+    pictogram: '06_toilet.png',
+  },
+  doctor: {
+    desc: 'Doctor',
+    pictogram: '07_doctor.png',
+  },
+  bandage: {
+    desc: 'Bandage',
+    pictogram: '08_bandage.png',
+  },
+  nausea: {
+    desc: 'Nausea',
+    pictogram: '09_nausea.png',
+  },
+  walk: {
+    desc: 'Walk',
+    pictogram: '10_walk.png',
+  },
+  question: {
+    desc: 'Question',
+    pictogram: '11_question.png',
+  },
+  media: {
+    desc: 'Media',
+    pictogram: '12_media.png',
+  },
+  hygiene: {
+    desc: 'Hygiene',
+    pictogram: '13_hygiene.png',
+  },
+  getup: {
+    desc: 'Getup',
+    pictogram: '14_getup.png',
+  },
+  window: {
+    desc: 'Window',
+    pictogram: '15_window.png',
+  },
+  cold: {
+    desc: 'Cold',
+    pictogram: '16_cold.png',
+  },
+  hot: {
+    desc: 'hot',
+    pictogram: '17_hot.png',
+  },
+  batterylow: {
+    desc: 'Batterylow',
+    pictogram: '18_batterylow.png',
+  },
+  emergency: {
+    desc: 'Emergency',
+    pictogram: 'emergency.png',
+  } 
+
 };
