0fa9336ad20ca86a97542bc9de1532fefd076aa8
Jerry <jerry_agboola@hotmail.com>
2024-03-21 07:48:39 +0100

    Update patient.tsx

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 import { Page } from 'framework7-react';
 import NeedButtonGrid from '../components/need-button-grid/need-button-grid.component.tsx';
 
-export const BedContext = createContext('103.1');
+export const BedContext = createContext('103:1');
 
 const PatientPage = () => {
   return (
